dd2c7a4c735e00e6cc0a2db6f68ef3eddc433754
gabrielrsp <gabrielrsp18@gmail.com>
2020-05-03 15:05:52 -0300

    add type validation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -16,6 +16,9 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
